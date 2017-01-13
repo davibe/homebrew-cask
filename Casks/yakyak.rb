d2fe7b407306459c680f73ced471f89a2aeafed1
Davide Bertola <dade@dadeb.it>
2017-01-13 08:05:41 +0100

    update sha for yakyak
    The released project binaries were wrongly updated and the `sha` changed.
    This is the correct one as stated in https://github.com/yakyak/yakyak/issues/550.

--- Casks/yakyak.rb
+++ Casks/yakyak.rb
@@ -1,6 +1,6 @@
 cask 'yakyak' do
   version '1.4.1'
-  sha256 '84ea0423d7262df947a3ae9162c75ba7f0d5d5451fd05e6e13bd8c921c76bacb'
+  sha256 '8a65fbb2b15d9805341571a2d7aed60f68e2839932b77706fb6363b9bfc1c5e9'
 
   url "https://github.com/yakyak/yakyak/releases/download/v#{version}/yakyak-#{version}-osx.zip"
   appcast 'https://github.com/yakyak/yakyak/releases.atom',
